862ce3fad0f0b02ddb3e9e3d48ffdf828a986803
pradeep <pradeep.kumar44@gmail.com>
2019-07-16 12:13:35 +0530

    initial commit

---
+++ src/main/java/com/sapient/springdemo/controller/RegisterController.java
@@ -0,0 +1,12 @@
+package com.sapient.springdemo.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class RegisterController {
+
+
+    public String login(){
+        return "login";
+    }
+}
