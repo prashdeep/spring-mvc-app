afc4145c5c9ec7fdc9dc6a8c7ea75149a1f1cc1a
pradeep <pradeep.kumar44@gmail.com>
2019-07-16 16:45:26 +0530

    committing the changes

--- src/main/java/com/sapient/springdemo/controller/RegisterController.java
+++ src/main/java/com/sapient/springdemo/controller/RegisterController.java
@@ -1,8 +1,13 @@
 package com.sapient.springdemo.controller;
 
+import com.sapient.springdemo.model.Employee;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.List;
+import java.util.ArrayList;
 
 @Controller
 @RequestMapping("/employee")
@@ -10,6 +15,25 @@ public class RegisterController {
 
     @GetMapping("/login")
     public String login(){
+
         return "login";
+    }
+
+    @GetMapping("/listAll")
+    public String displayAllEmployees(Model model){
+        List<Employee> empList = new ArrayList<>();
+        empList.add(new Employee(1, "Vinay"));
+        empList.add(new Employee(2, "Harish"));
+        model.addAttribute("empList", empList);
+        return "display";
+    }
+
+    @GetMapping("/{id}")
+    public String displayEmployee(@PathVariable("id") int id,  Model model){
+
+        Employee employee = new Employee(id, "Vinay");
+
+        model.addAttribute("employee", employee);
+        return "employee";
     }
 }
